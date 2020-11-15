cc52c091ded262d81ef61d7887ee6819af4faf0d
avttrue <truetrifonov@yandex.ru>
2020-11-15 16:11:39 +0300

    design

--- dialogs/dialogbody.h
+++ dialogs/dialogbody.h
@@ -5,14 +5,14 @@
 #include <QLabel>
 
 const QString DB_CAPTION_STYLE = "background: qlineargradient(x1:0, y1:0, x2:0, y2:1, "
-                                 "stop: 0 Silver, stop: 0.5 PowderBlue, "
-                                 "stop: 0.5 PowderBlue, stop: 1 silver); "
-                                 "border: 1px solid Silver; "
+                                 "stop: 0 #C0C0C0, stop: 0.5 #B0E0E6, "
+                                 "stop: 0.5 #B0E0E6, stop: 1 #C0C0C0); "
+                                 "border: 1px solid #C0C0C0; "
                                  "padding: 0px 1px 0px 1px; border-radius: 5px; "
                                  "color: black; font-family: monospace; font: bold; ";
 
 const QString DB_ICON_STYLE = "background: qradialgradient(cx:0.5, cy:0.5, radius: 1, "
-                              "fx:0.5, fy:0.5, stop:0 white, stop:1 silver); "
+                              "fx:0.5, fy:0.5, stop:0 white, stop:1 #C0C0C0); "
                               "border-radius: 5px; "
                               "padding: 1px; "
                               "margin: 1px; ";
@@ -20,7 +20,7 @@ const QString DB_ICON_STYLE = "background: qradialgradient(cx:0.5, cy:0.5, radiu
 const QString DB_WINDOW_STYLE = "QDialog { border-style: ridge; "
                                 "border-width: 2px; "
                                 "border-radius: 5px; "
-                                "border-color: silver; }";
+                                "border-color: #C0C0C0; }";
 
 class QGridLayout;
 class QToolBar;
