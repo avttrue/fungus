cfbe086c0a838bbaa63676c1e914ba1f55076a6c
avttrue <truetrifonov@yandex.ru>
2020-12-08 09:23:19 +0300

    edit; version

--- main.cpp
+++ main.cpp
@@ -46,6 +46,8 @@ void createDirectories()
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication application(argc, argv);
     application.setStyleSheet(getTextFromRes(":/resources/main.css"));
 
